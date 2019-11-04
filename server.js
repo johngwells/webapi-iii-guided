3f0125664fd25ca170bc54f1fb176e206b186dc5
John Wells <molekewl2017@gmail.com>
2019-11-04 03:17:17 -0800

    middleware logger & morgan

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
@@ -12,11 +13,23 @@ function dateLogger(req, res, next) {
   next();
 }
 
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`)
+  
+  next();
+}
+
+function gateKeeper(req, res, next) {
+  const password = req.headers.password;
+}
+
 // Global Middleware * Runs on every request
 server.use(helmet()); // third party
 
 server.use(express.json());
 server.use(dateLogger);
+server.use(logger);
+server.use(morgan('dev'));
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
